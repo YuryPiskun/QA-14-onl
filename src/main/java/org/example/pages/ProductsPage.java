ddb9dc0ae0a10313af7c00de472175b9dd055dca
Yury Piskun <singleshooting@gmail.com>
2022-07-04 00:19:45 +0300

    some edits

--- src/main/java/org/example/pages/ProductsPage.java
+++ src/main/java/org/example/pages/ProductsPage.java
@@ -8,16 +8,12 @@ public class ProductsPage extends BasePage {
 
     private final By TITLE = By.cssSelector(".title");
     private final String addToCartButton = "//div[text()='%s']/ancestor::div[@class='inventory_item']//button";
-
-
     public ProductsPage(WebDriver driver) {
         super(driver);
     }
-
     public void open() {
         driver.get(BASE_URL + "inventory.html");
     }
-
     public WebElement getTitle(){
         return driver.findElement(TITLE);
     }
