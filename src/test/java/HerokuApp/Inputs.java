5f9d6213f6875fec1a5267fb6c01fc83a93c3f2f
Yury Piskun <singleshooting@gmail.com>
2022-07-04 00:06:50 +0300

    create Typos and Input tasks

---
+++ src/test/java/HerokuApp/Inputs.java
@@ -0,0 +1,32 @@
+package HerokuApp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Inputs extends HerokuApp {
+    @BeforeMethod
+    public void DropdownLink() {
+        WebElement dropdownLink = driver.findElement(By.xpath
+                ("//a[text()='Inputs']"));
+        dropdownLink.click();
+    }
+
+    @Test
+    public void InputText() {
+        WebElement field = driver.findElement(By.xpath("//input[@type='number']"));
+        String text = "hello";
+        field.sendKeys(text);
+    }
+
+    @Test
+    public void InputNumbers() {
+        WebElement field = driver.findElement(By.xpath("//input[@type='number']"));
+        field.sendKeys(Keys.ARROW_UP);
+        field.sendKeys(Keys.ARROW_UP);
+        field.sendKeys(Keys.ARROW_UP);
+        field.sendKeys(Keys.ARROW_DOWN);
+    }
+}
---
+++ src/test/java/HerokuApp/Typos.java
@@ -0,0 +1,23 @@
+package HerokuApp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Typos extends HerokuApp{
+    @BeforeMethod
+    public void DropdownLink() {
+        WebElement dropdownLink = driver.findElement(By.xpath
+                ("//a[text()='Typos']"));
+        dropdownLink.click();
+    }
+    @Test
+    public void CheckText(){
+        List<WebElement> pageText = driver.findElements(By.tagName("p"));
+        Assert.assertEquals(pageText.get(1).getText(),"Sometimes you'll see a typo, other times you won't.");
+    }
+}
